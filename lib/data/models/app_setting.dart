34b444faecca420dc7452c77a9a744cd60a9eb2e
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2026-01-30 15:14:39 +0700

    created model app setting

---
+++ lib/data/models/app_setting.dart
@@ -0,0 +1,23 @@
+import 'package:equatable/equatable.dart';
+
+class AppSetting extends Equatable {
+  final String key;
+  final String value;
+
+  const AppSetting({required this.key, required this.value});
+
+  Map<String, dynamic> toMap() {
+    return {'key': key, 'value': value};
+  }
+
+  factory AppSetting.formMap(Map<String, dynamic> map) {
+    return AppSetting(key: map['key'] as String, value: map['value'] as String);
+  }
+
+  AppSetting copyWith({String? key, String? value}) {
+    return AppSetting(key: key ?? this.key, value: value ?? this.value);
+  }
+
+  @override
+  List<Object?> get props => [key, value];
+}
