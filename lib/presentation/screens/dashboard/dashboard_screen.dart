1daa905ac9a70fecc995e91160887fd7aac088ce
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2026-02-05 10:45:32 +0700

    created cubit dashboard

--- lib/presentation/screens/dashboard/dashboard_screen.dart
+++ lib/presentation/screens/dashboard/dashboard_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:laundry_application/core/theme/app_theme.dart';
+import 'package:laundry_application/logic/cubits/dashboard/dashboard_cubit.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
@@ -9,6 +10,20 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class _DashboardScreenState extends State<DashboardScreen> {
+  late DashboardCubit _dashboardCubit;
+
+  @override
+  void initState() {
+    super.initState();
+    _dashboardCubit = DashboardCubit()..loadDashboard();
+  }
+
+  @override
+  void dispose() {
+    _dashboardCubit.close();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
